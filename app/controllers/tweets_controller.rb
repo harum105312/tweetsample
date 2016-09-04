915dd7d7e4ec9f8b251c728fc5d976fca4c12062
Nonscale <info@nonscale.com>
2016-09-04 02:27:05 +0000

    createアクションの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,15 @@ class TweetsController < ApplicationController
 		@tweets = Tweet.all
 		@tweet = Tweet.new
 	end
+
+	def create
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save
+		redirect_to tweets_path
+	end
+
+	private
+	def tweet_params
+		params.require(:tweet).permit(:body)
+	end
 end
